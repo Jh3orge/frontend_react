bb95fb3be5d78858ee9a03961eb12438012e4286
YorchCH <jorgelgx@hotmail.com>
2022-08-18 22:33:28 -0500

    images & navbarFirstSteps

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
 import './Navbar.scss';
+import { images } from '../../constants';
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.adidas} alt="logo" width="400px"/>
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'skills', 'projects', 'testimonials', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))};
+      </ul>
+    </nav>
   )
 }
 
